82136552545ab0ce63119668cd8815ed0b335661
Chase Adams <c@cadams.io>
2022-05-31 13:09:22 -0700

    fix: added tracking of transcoder pool fees (#46)

--- src/mappings/roundsManager.ts
+++ src/mappings/roundsManager.ts
@@ -14,7 +14,6 @@ import {
   getTimestampForDaysPast,
   makeEventId,
   makePoolId,
-  ONE_BD,
   PERC_DIVISOR,
   ZERO_BD,
 } from "../../utils/helpers";
@@ -118,6 +117,7 @@ export function newRound(event: NewRound): void {
     pool = new Pool(poolId);
     pool.round = round.id;
     pool.delegate = currentTranscoder.toHex();
+    pool.fees = ZERO_BD;
     if (transcoder) {
       pool.totalStake = transcoder.totalStake;
       pool.rewardCut = transcoder.rewardCut;
