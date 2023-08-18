c6b97bfc83e37c7ae9238f2874cd7051d56697c6
Victor Elias <victorgelias@gmail.com>
2023-08-18 18:52:18 -0300

    mappings: Fix TS syntax for CI

--- src/mappings/treasury.ts
+++ src/mappings/treasury.ts
@@ -2,15 +2,15 @@ import { TreasuryProposal } from "../types/schema";
 import { ProposalCreated } from "../types/Treasury/LivepeerGovernor";
 
 export function proposalCreated(event: ProposalCreated): void {
-  const { proposalId, proposer, targets, values, calldatas, voteStart, voteEnd, description } = event.params;
+  const p = event.params;
 
-  const proposal = new TreasuryProposal(proposalId.toString());
-  proposal.proposer = proposer.toHex();
-  proposal.targets = targets.map<string>((t) => t.toHex());
-  proposal.values = values;
-  proposal.calldatas = calldatas;
-  proposal.voteStart = voteStart;
-  proposal.voteEnd = voteEnd;
-  proposal.description = description;
+  const proposal = new TreasuryProposal(p.proposalId.toString());
+  proposal.proposer = p.proposer.toHex();
+  proposal.targets = p.targets.map<string>((t) => t.toHex());
+  proposal.values = p.values;
+  proposal.calldatas = p.calldatas;
+  proposal.voteStart = p.voteStart;
+  proposal.voteEnd = p.voteEnd;
+  proposal.description = p.description;
   proposal.save();
 }
